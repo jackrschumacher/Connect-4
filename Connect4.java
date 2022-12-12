7d9a7b20c5fcd3e84c5b86fe8b296e80cbdc34f6
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-12-12 17:31:29 +0000

    Finish displayBoard() function

--- Connect4.java
+++ Connect4.java
@@ -36,9 +36,24 @@ public class Connect4{
 
 	private void displayBoard(){
 		// Displays the ASCII art for the board and displays the charachters at that specific section of the board.
+		// Use two spaces for the board println before the "|"
 		System.out.println("    |    |    |    |    |    |    |");
-		System.out.println(" "+ board[0][0]+ " | "+ board[0][1]+ " | "+ board[0][2]+ " | "+ board[0][3]+ " | "+ board[0][4]+ " | " + board[0][5] + " | "+ board[0][6]);
+		System.out.println(" "+ board[0][0]+ "  | "+ board[0][1]+ "  | "+ board[0][2]+ "  | "+ board[0][3]+ "  | "+ board[0][4]+ "  | " + board[0][5] + "  | "+ board[0][6] + "  | ");
+		System.out.println("____|____|____|____|____|____|____|");
 		System.out.println("    |    |    |    |    |    |    |");
-	}
+		System.out.println(" "+ board[1][0]+ "  | "+ board[1][1]+ "  | "+ board[1][2]+ "  | "+ board[1][3]+ "  | "+ board[1][4]+ "  | " + board[1][5] + "  | "+ board[1][6] + "  | ");
+		System.out.println("____|____|____|____|____|____|____|");
+		System.out.println("    |    |    |    |    |    |    |");
+		System.out.println(" "+ board[2][0]+ "  | "+ board[2][1]+ "  | "+ board[2][2]+ "  | "+ board[2][3]+ "  | "+ board[2][4]+ "  | " + board[2][5] + "  | "+ board[2][6] + "  | ");
+		System.out.println("____|____|____|____|____|____|____|");
+		System.out.println("    |    |    |    |    |    |    |");
+		System.out.println(" "+ board[3][0]+ "  | "+ board[3][1]+ "  | "+ board[3][2]+ "  | "+ board[3][3]+ "  | "+ board[3][4]+ "  | " + board[3][5] + "  | "+ board[3][6] + "  | ");
+		System.out.println("____|____|____|____|____|____|____|");
+		System.out.println("    |    |    |    |    |    |    |");
+		System.out.println(" "+ board[4][0]+ "  | "+ board[4][1]+ "  | "+ board[4][2]+ "  | "+ board[4][3]+ "  | "+ board[4][4]+ "  | " + board[4][5] + "  | "+ board[4][6] + "  | ");
+		System.out.println("____|____|____|____|____|____|____|");
+		System.out.println("    |    |    |    |    |    |    |");
+		System.out.println(" "+ board[5][0]+ "  | "+ board[5][1]+ "  | "+ board[5][2]+ "  | "+ board[5][3]+ "  | "+ board[5][4]+ "  | " + board[5][5] + "  | "+ board[5][6] + "  | ");
+		System.out.println("____|____|____|____|____|____|____|");
 	}
 }
