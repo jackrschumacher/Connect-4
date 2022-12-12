9cea966523b5e622597c667feb87cdb0d760126c
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-12-12 17:17:15 +0000

    Start setting up displayBoard() function

--- Connect4.java
+++ Connect4.java
@@ -6,13 +6,39 @@ public class Connect4{
 // Set up board
 	private char [][] board;
 	private int turn;
+	
 
 	
 	public Connect4(){
 		turn  = 0;
 	}
-}
 
-public void playGame(){
-	
+
+	public void playGame(){
+		Scanner in = new Scanner(System.in);
+		createBoard();
+		displayBoard();
+		
+		return;
+		
+	}
+
+	private void createBoard(){
+		this.board = new char[][]{
+			{' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		};
+		}
+
+	private void displayBoard(){
+		// Displays the ASCII art for the board and displays the charachters at that specific section of the board.
+		System.out.println("    |    |    |    |    |    |    |");
+		System.out.println(" "+ board[0][0]+ " | "+ board[0][1]+ " | "+ board[0][2]+ " | "+ board[0][3]+ " | "+ board[0][4]+ " | " + board[0][5] + " | "+ board[0][6]);
+		System.out.println("    |    |    |    |    |    |    |");
+	}
+	}
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
   public static void main(String[] args) {
     Connect4 game = new Connect4();
+		game.playGame();
   }
+}
 
