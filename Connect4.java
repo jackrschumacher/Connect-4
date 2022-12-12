27e1ba232d1810a4b7587e27d3fb87d8afda0a82
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-12-12 17:01:32 +0000

    Setup Variables and Call Functions

---
+++ Connect4.java
@@ -0,0 +1,18 @@
+// Import everything in java.util
+import java.util.*;
+
+public class Connect4{
+// Set up class and methods
+// Set up board
+	private char [][] board;
+	private int turn;
+
+	
+	public Connect4(){
+		turn  = 0;
+	}
+}
+
+public void playGame(){
+	
+}
