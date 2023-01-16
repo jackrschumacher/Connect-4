a5e81fd76bec75fa56f81fa3825b31a29b223c5f
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2023-01-16 01:14:36 +0000

    ASCII Formatting Fixes

--- Connect4.java
+++ Connect4.java
@@ -66,10 +66,11 @@ public class Connect4 {
 
 	private boolean placeAvaliable(int row, int col) {
 		boolean avaliable = true;
+		System.out.println(board[row][col]);
 		if (board[row][col] != ' ') {
 			avaliable = false;
 		}
-		if (avaliable = true) {
+		else{
 			if (turn == 0){
 				board[row][col] = '1';
 				turn = -1;
@@ -167,29 +168,29 @@ public class Connect4 {
 		// specific section of the board.
 		// Board is 6*7
 		// Use two spaces for the board println before the "|"
-		System.out.println("    |    |    |    |    |    |    |");
-		System.out.println(" " + board[0][0] + "  | " + board[0][1] + "  | " + board[0][2] + "  | " + board[0][3] + "  | "
+		System.out.println("|    |    |    |    |    |    |    |");
+		System.out.println("| " + board[0][0] + "  | " + board[0][1] + "  | " + board[0][2] + "  | " + board[0][3] + "  | "
 				+ board[0][4] + "  | " + board[0][5] + "  | " + board[0][6] + "  | ");
-		System.out.println("____|____|____|____|____|____|____|");
-		System.out.println("    |    |    |    |    |    |    |");
-		System.out.println(" " + board[1][0] + "  | " + board[1][1] + "  | " + board[1][2] + "  | " + board[1][3] + "  | "
+		System.out.println("|____|____|____|____|____|____|____|");
+		System.out.println("|    |    |    |    |    |    |    |");
+		System.out.println("| " + board[1][0] + "  | " + board[1][1] + "  | " + board[1][2] + "  | " + board[1][3] + "  | "
 				+ board[1][4] + "  | " + board[1][5] + "  | " + board[1][6] + "  | ");
-		System.out.println("____|____|____|____|____|____|____|");
-		System.out.println("    |    |    |    |    |    |    |");
-		System.out.println(" " + board[2][0] + "  | " + board[2][1] + "  | " + board[2][2] + "  | " + board[2][3] + "  | "
+		System.out.println("|____|____|____|____|____|____|____|");
+		System.out.println("|    |    |    |    |    |    |    |");
+		System.out.println("| " + board[2][0] + "  | " + board[2][1] + "  | " + board[2][2] + "  | " + board[2][3] + "  | "
 				+ board[2][4] + "  | " + board[2][5] + "  | " + board[2][6] + "  | ");
-		System.out.println("____|____|____|____|____|____|____|");
-		System.out.println("    |    |    |    |    |    |    |");
-		System.out.println(" " + board[3][0] + "  | " + board[3][1] + "  | " + board[3][2] + "  | " + board[3][3] + "  | "
+		System.out.println("|____|____|____|____|____|____|____|");
+		System.out.println("|    |    |    |    |    |    |    |");
+		System.out.println("| " + board[3][0] + "  | " + board[3][1] + "  | " + board[3][2] + "  | " + board[3][3] + "  | "
 				+ board[3][4] + "  | " + board[3][5] + "  | " + board[3][6] + "  | ");
-		System.out.println("____|____|____|____|____|____|____|");
-		System.out.println("    |    |    |    |    |    |    |");
-		System.out.println(" " + board[4][0] + "  | " + board[4][1] + "  | " + board[4][2] + "  | " + board[4][3] + "  | "
+		System.out.println("|____|____|____|____|____|____|____|");
+		System.out.println("|    |    |    |    |    |    |    |");
+		System.out.println("| " + board[4][0] + "  | " + board[4][1] + "  | " + board[4][2] + "  | " + board[4][3] + "  | "
 				+ board[4][4] + "  | " + board[4][5] + "  | " + board[4][6] + "  | ");
-		System.out.println("____|____|____|____|____|____|____|");
-		System.out.println("    |    |    |    |    |    |    |");
-		System.out.println(" " + board[5][0] + "  | " + board[5][1] + "  | " + board[5][2] + "  | " + board[5][3] + "  | "
+		System.out.println("|____|____|____|____|____|____|____|");
+		System.out.println("|    |    |    |    |    |    |    |");
+		System.out.println("| " + board[5][0] + "  | " + board[5][1] + "  | " + board[5][2] + "  | " + board[5][3] + "  | "
 				+ board[5][4] + "  | " + board[5][5] + "  | " + board[5][6] + "  | ");
-		System.out.println("____|____|____|____|____|____|____|");
+		System.out.println("|____|____|____|____|____|____|____|");
 	}
 }
